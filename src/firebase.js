b16109e5660f47539ee5a38048e8547bc5750946
Hassan Jamal <hassanjamal6577987@gmail.com>
2025-04-20 13:54:25 +0500

    Blog Feature Included.

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // ✅ Make sure this is imported
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAz8beNrkSHrIXZRjHpAWjDRa0hMJMk2F4",
-  authDomain: "mern-ui.firebaseapp.com",
-  projectId: "mern-ui",
-  storageBucket: "mern-ui.appspot.com",
-  messagingSenderId: "388740603116",
-  appId: "1:388740603116:web:1a36eb1f779dec823299c9",
+ 
 };
 
 const app = initializeApp(firebaseConfig);
